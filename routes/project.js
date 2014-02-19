141d9a189444acc5b8fe394087fa4993ef9c0b34
Avi Bagla <avibagla@gmail.com>
2014-02-19 14:41:04 -0800

    put together everything with mongo

--- routes/project.js
+++ routes/project.js
@@ -2,12 +2,16 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
+  console.log("project id : " + projectID);
 
   // query for the specific project and
   // call the following callback
 
+  models.Project.find({"_id":projectID}).sort().exec(afterQuery);
+
   function afterQuery(err, projects) {
     if(err) console.log(err);
+
     res.json(projects[0]);
   }
 }
@@ -15,6 +19,19 @@ exports.projectInfo = function(req, res) { 
 exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
+  var newProject = models.Project({
+    "title": form_data.project_title,
+    "date": new Date(form_data.date),
+    "summary": form_data.summary,
+    "image":form_data.image_url
+  });
+
+  newProject.save(afterSaving);
+
+  function afterSaving(err){
+    if(err){console.log(err); res.send(500)};
+    res.redirect('/');
+  }
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
@@ -22,6 +39,12 @@ exports.addProject = function(req, res) {
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+  models.Project.find({"_id" : projectID}).remove().exec(afterRemoving);
+
+  function afterRemoving(err){
+    if(err) console.log(err);
+    res.send();
+  }
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
